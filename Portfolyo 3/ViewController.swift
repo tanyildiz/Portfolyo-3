8c109e7151a47b5e6e49b20788c655f9671d399d
niyazi <niyazi@niyazi.local>
2017-05-30 17:59:14 +0300

    first commit

--- Portfolyo 3/ViewController.swift
+++ Portfolyo 3/ViewController.swift
@@ -1,25 +1,99 @@
 //
 //  ViewController.swift
-//  Portfolyo 3
+//  Portfolyo
 //
-//  Created by niyazi on 30/05/2017.
-//  Copyright © 2017 tanyildiz. All rights reserved.
+//  Created by Niyazi TANYILDIZ on 29/05/2017.
+//  Copyright © 2017 nt. All rights reserved.
 //
 
 import UIKit
+import SDWebImage
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var workNames = [String]()
+    var images = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlStr = "http://www.tanyildiz.com/?json=1"
+        let url = URL(string: urlStr)
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            
+            if let urlVeri = data
+            {
+                do
+                {
+                    let json = try JSONSerialization.jsonObject(with: urlVeri, options: .mutableContainers) as? NSDictionary
+                    
+                    if let posts = json?.value(forKeyPath: "posts.title") as? NSArray
+                    {
+                        for i in 0..<posts.count {
+                            if let allpost = posts[i] as? String
+                            {
+                                self.workNames.append(allpost)
+                            }
+                        }
+                    }
+                    
+                    if let allimg = json?.value(forKeyPath: "posts.attachments.images.medium.url") as? NSArray
+                    {
+                        for i in 0..<allimg.count
+                        {
+                            self.images.append(allimg[i] as! String)
+                        }
+                    }
+                    
+                    
+                    
+                    // YORUM 1:  Burayı eklemezseniz tabloya veri doldurmaz. AŞAĞIDA 2 yorum daha yazdım
+                    DispatchQueue.main.async
+                        {
+                            self.tableView.reloadData()
+                    }
+                }
+                catch
+                {
+                    print(error)
+                }
+            }
+            
+        }
+        //YORUM 2:Task resume yanlış yerde kullanılmıştı Xcode öncesinde ; öneriyorsa bilinki hata yapmışsınızdır
+        task.resume()
+        print(self.images)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return workNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        //YORUM 3: Resim alanını burda tanımlamayıp tasarım ekranında tanımlarsanız uygulamanız çalışmaz.
+        //Resim olayı için kampanyalar derslerinin tamamını izleyin
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sampleCell", for: indexPath) as! SampleTableViewCell
+        
+        cell.workName.text = workNames[indexPath.row]
+        
+        let imgView = cell.viewWithTag(1) as? UIImageView
+        imgView?.sd_setImage(with: URL(string: images[indexPath.row]))
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
 }
 
---
+++ SampleTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  SampleTableViewCell.swift
+//  Portfolyo
+//
+//  Created by Niyazi TANYILDIZ on 29/05/2017.
+//  Copyright © 2017 nt. All rights reserved.
+//
+
+import UIKit
+
+class SampleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBOutlet weak var workName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
