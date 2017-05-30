d35b4c6a21c5bb18b124312832b63613b9243ae1
niyazi <niyazi@niyazi.local>
2017-05-30 17:45:32 +0300

    Initial Commit

---
+++ Portfolyo 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Portfolyo 3
+//
+//  Created by niyazi on 30/05/2017.
+//  Copyright © 2017 tanyildiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
